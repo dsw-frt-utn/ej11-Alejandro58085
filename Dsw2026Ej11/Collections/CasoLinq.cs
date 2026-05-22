e35eec7a51a713c078fc7401a230c930062b4913
Alejandro <exmen17180501alejan@gmail.com>
2026-05-22 13:59:24 -0300

    Se realizaron cambios en CasoLinq

--- Dsw2026Ej11/Collections/CasoLinq.cs
+++ Dsw2026Ej11/Collections/CasoLinq.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 /*
  * Para cada punto crear un método que permita:
@@ -16,4 +18,50 @@
  */
 public class CasoLinq
 {
+    private List<Libro> libros = new List<Libro>();
+    public void agregarLibro(Libro libro)
+    {
+        libros.Add(libro);
+    }
+    public Libro GetPrimero()
+    {
+        return libros.First();
+    }
+    public Libro GetUltimo()
+    {
+        return libros.Last();
+    }
+    public decimal GetTotalPrecios()
+    {
+        return libros.Sum(l => l.Precio);
+    }
+    public decimal GetPromedioPrecios()
+    {
+        return libros.Average(promedio => promedio.Precio);
+    }
+    public List<Libro> GetListById()
+    {
+        return libros.Where(idMayor=> idMayor.Id> 15).ToList();
+    }
+    public List<string> Getibros()
+    {
+        return libros.Select(libro=>$"{libro.Titulo}-{libro.Precio:C}").ToList();
+    }
+    public Libro? GetMayorPrecio()
+    {
+        return libros.MaxBy(libro => libro.Precio);
+    }
+    public Libro? GetMenorPrecio()
+    {
+        return libros.MinBy(libro => libro.Precio);
+    }
+    public List<Libro> GetMayorProedio()
+    {
+        decimal promedio = libros.Average(l => l.Precio);
+        return libros.Where(libro => libro.Precio > promedio).ToList();
+    }
+    public List<Libro> GetOrdenadosPorTitulo()
+    {
+        return libros.OrderByDescending(libro => libro.Titulo).ToList();
+    }
 }
